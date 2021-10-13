2ea41cea7c79ee52ca76230faf156af67027aea5
Kevinvazquezg <vgk960401@gmail.com>
2021-10-13 18:27:37 -0500

    Reestructura de carpetas

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
-const sequileze = require('./db/conexion');
+const sequileze = require('./back/db/conexion');
 const cookieParser = require('cookie-parser')
 const app = express();
-const indexView = require('./view/indexView.js')
+const indexView = require('./back/view/indexView.js')
 app.use(express.json())
 
 app.use(cookieParser())
