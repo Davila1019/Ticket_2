72e7e94ae019cb7305e5074fb8aee702729e4eaa
AleAlvanez <42542259+AleAlvanez@users.noreply.github.com>
2021-10-16 19:58:41 -0500

    endpoint de agregar datos

--- back/view/indexView.js
+++ back/view/indexView.js
@@ -5,4 +5,9 @@ module.exports = async (app) => {
     app.get('/',isAuthenticated.isAuthenticated,async(req,res) => {
         res.render('index')
     });
+    
+     app.get('/agregarInfo',async(req,res) => {
+        res.render('formDatos')
+    });
 };
+
