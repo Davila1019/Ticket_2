74616d5d6a68dc98727f1617733a015e30561783
AleAlvanez <42542259+AleAlvanez@users.noreply.github.com>
2021-10-17 19:02:47 -0500

    Endpoint de editar info

--- back/view/indexView.js
+++ back/view/indexView.js
@@ -9,5 +9,9 @@ module.exports = async (app) => {
      app.get('/agregarInfo',async(req,res) => {
         res.render('formDatos')
     });
+    
+     app.get('/editInfo',async(req,res) => {
+        res.render('editInfo')
+    });
 };
 
